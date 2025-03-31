bd4f8f7209c7e031fc1bc6920198ac6687814e25
heheWelp <106212414+heheWelp@users.noreply.github.com>
2025-03-30 21:50:12 -0500

    Delete src/pages directory

--- src/pages/_app.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import type { AppProps } from 'next/app'
-import '../app/globals.css'
-
-function MyApp({ Component, pageProps }: AppProps): React.ReactElement {
-  return <Component {...pageProps} />
-}
-
-export default MyApp 
